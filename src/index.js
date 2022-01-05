9bb7d17fd31643940082e2823782e1ecd2578608
Edgar <cheesefome@gmail.com>
2022-01-05 16:11:23 -0500

    First Commit

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const name = "Edgar";
-const currentDate = new Date();
-const year = currentDate.getFullYear();
-
 ReactDOM.render(
   <div>
-    <p>Created by {name}</p>
-    <p>Copyright {year}</p>
+    <h1>Favorite Foods</h1>,
+    <ul>
+      <li>Pizza</li>
+      <li>Chinese</li>
+      <li>Ramen</li>
+    </ul>
   </div>,
+
   document.getElementById("root")
 );
 
@@ -20,13 +21,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
-// Create a react app from scratch
-// It should display 2 paragraph HTML Elements
-// The paragraphs should say:
-// Created by YOURNAME.
-// Copyright CURRENTYEAR.
-// E.g.
-// Created by Edgar Ham.
-// Copyright 2019.
