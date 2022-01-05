b8d6b9ef5f49f10924083284c64659263a95d6a5
Edgar <cheesefome@gmail.com>
2022-01-05 16:03:49 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -4,14 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const name = "Edgar";
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+    <p>Created by {name}</p>
+    <p>Copyright {year}</p>
+  </div>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// Create a react app from scratch
+// It should display 2 paragraph HTML Elements
+// The paragraphs should say:
+// Created by YOURNAME.
+// Copyright CURRENTYEAR.
+// E.g.
+// Created by Edgar Ham.
+// Copyright 2019.
