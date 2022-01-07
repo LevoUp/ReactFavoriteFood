c065dbb77f54dd6b1dec35977604bac0b5be25c0
Edgar <cheesefome@gmail.com>
2022-01-07 13:31:18 -0500

    Learned To Use If Statements With JSX and React

--- src/index.js
+++ src/index.js
@@ -4,18 +4,37 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const customStyle = {
-  color: "red",
-  border: "dotted",
-  fontSize: "24px"
+// Create a React app from scratch.
+// Show a single h1 that says "Good Morning" if between midnight and 12pm.
+// or "Good Afternoon" if between 6pm and midnight.
+// Apply the "heading" style in the Styles.css.
+// Dynamically change the color of the h1 using inline css styles.
+// Morning = red, Afternoon = green, Night = blue.
+
+const currentHour = new Date().getHours();
+const colors = {
+  color: "",
+  greeting: ""
 }
 
-customStyle.color = "purple";
-
-ReactDOM.render(
-  <h1 style={customStyle}>Hello World!</h1>,
+if (currentHour >= 0 && currentHour < 12) {
+  colors.color = "red";
+  ReactDOM.render(
+    <h1 className="heading" style={colors}>Good Morning!</h1>,
+    document.getElementById("root")
+  );
+} else if (currentHour >= 12 && currentHour < 19) {
+  colors.color = "green";
+  ReactDOM.render(
+    <h1 className="heading" style={colors}>Good Afternoon!</h1>,
+    document.getElementById("root")
+  );
+} else {
+  colors.color = "blue";
+  ReactDOM.render(<h1 className="heading" style={colors}>Good Night!</h1>,
   document.getElementById("root")
-);
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
