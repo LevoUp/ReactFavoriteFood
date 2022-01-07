a55e2c5192b683653abce4a46224b6e30d001385
Edgar <cheesefome@gmail.com>
2022-01-07 14:32:41 -0500

    Learned Importing and Exporting JSX/Components

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./Heading.jsx";
+import List from "./List.jsx";
+
+function myApp() {
+    return (
+    <div>
+        <Heading/>
+        <List/>
+    </div>
+    );
+}
+
+export default myApp;
--- src/index.js
+++ src/index.js
@@ -3,34 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-// Create a React app from scratch.
-// Show a single h1 that says "Good Morning" if between midnight and 12pm.
-// or "Good Afternoon" if between 6pm and midnight.
-// Apply the "heading" style in the Styles.css.
-// Dynamically change the color of the h1 using inline css styles.
-// Morning = red, Afternoon = green, Night = blue.
-
-const currentTime = new Date().getHours();
-
-let greeting;
-const customStyle = {
-  color: ""
-}
-
-if (currentTime < 12) {
-  greeting = "Good Morning!"
-  customStyle.color = "red"
-} else if (currentTime < 18 ) {
-  greeting = "Good Afternoon!"
-  customStyle.color = "green"
-} else {
-  greeting = "Good Night!"
-  customStyle.color = "blue"
-}
+import EntireApp from "./components/App.jsx";
 
 ReactDOM.render(
-  <h1 className="heading" style={customStyle}>{greeting}</h1>,
+  <EntireApp/>,
   document.getElementById("root")
 );
 
