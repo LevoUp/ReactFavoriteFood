3fb8117d023985763cd35a0c2971b01cd92c6372
Edgar <cheesefome@gmail.com>
2022-01-07 13:44:00 -0500

    Learned To Use If Statements With JSX and React(AngelaYue Version)

--- src/index.js
+++ src/index.js
@@ -11,30 +11,28 @@ import reportWebVitals from './reportWebVitals';
 // Dynamically change the color of the h1 using inline css styles.
 // Morning = red, Afternoon = green, Night = blue.
 
-const currentHour = new Date().getHours();
-const colors = {
-  color: "",
-  greeting: ""
+const currentTime = new Date().getHours();
+
+let greeting;
+const customStyle = {
+  color: ""
 }
 
-if (currentHour >= 0 && currentHour < 12) {
-  colors.color = "red";
-  ReactDOM.render(
-    <h1 className="heading" style={colors}>Good Morning!</h1>,
-    document.getElementById("root")
-  );
-} else if (currentHour >= 12 && currentHour < 19) {
-  colors.color = "green";
-  ReactDOM.render(
-    <h1 className="heading" style={colors}>Good Afternoon!</h1>,
-    document.getElementById("root")
-  );
+if (currentTime < 12) {
+  greeting = "Good Morning!"
+  customStyle.color = "red"
+} else if (currentTime < 18 ) {
+  greeting = "Good Afternoon!"
+  customStyle.color = "green"
 } else {
-  colors.color = "blue";
-  ReactDOM.render(<h1 className="heading" style={colors}>Good Night!</h1>,
-  document.getElementById("root")
-  );
+  greeting = "Good Night!"
+  customStyle.color = "blue"
 }
+
+ReactDOM.render(
+  <h1 className="heading" style={customStyle}>{greeting}</h1>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
